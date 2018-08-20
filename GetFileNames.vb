5e95d3fd7e87bfc5d89dbf9be56fa07d64c600f9
gcgdogo <gcgdogo@163.com>
2018-08-20 22:48:42 +0800

    2018-08-20

--- GetFileNames.vb
+++ GetFileNames.vb
@@ -1,23 +1,25 @@
-Sub Get_File_Names(FolderName as string , NameStyle as string , TargetTable as string)
-  
+Sub Get_File_Names(GroupName as string , FolderName as string , NameStyle as string , TargetTable as string)
+
   	Dim FileNames(500) as string , FileNamesCount as integer , ThisFileName as string
   	Dim ADO_rs as adodb.recordset
   	Set ADO_rs = new adodb.recordset
-    
+
     'Set:ADO_rs source=TargetTable
-  	
+
     'Do:ADO_rs.EOF FileNames()=ADO_rs!FileName
-    
+
     'Do:ThisFileName=dir(FolderName)
-    	
+
         'Check:RegExp (Thisfilename , NameStyle)
 
-   		'For:If:FileNames=Thisfilename ThisFileName=""
+ 		'For:If:FileNames=Thisfilename ThisFileName=""
+
+      	'ADO_rs add
+
+      	'FileName=ThisFileName
+
+      '123156
     
-    	'ADO_rs add
-    
-    	'FileName=ThisFileName
-    
-    
-    
+
+
 End sub
