f6e66c30c140c7b3741f5290831727a422c0d486
gcgdogo <gcgdogo@163.com>
2018-08-24 08:37:46 +0800

    2018-08-24 08:37

--- GetFileNames.vb
+++ GetFileNames.vb
@@ -1,24 +1,24 @@
 Sub Get_File_Names(GroupName as string , FolderName as string , NameStyle as string , TargetTable as string)
 
-  	Dim FileNames(500) as string , FileNamesCount as integer , ThisFileName as string
-  	Dim ADO_rs as adodb.recordset
-    Dim Chk_String as RegExp
-  	Set ADO_rs = new adodb.recordset
-    set Chk_String as new RegExp
+  Dim FileNames(500) as string , FileNamesCount as integer , ThisFileName as string
+  Dim ADO_rs as adodb.recordset
+  Dim Chk_String as RegExp
+  Set ADO_rs = new adodb.recordset
+  set Chk_String as new RegExp
 
-    'Set:ADO_rs source=TargetTable
+  'Set:ADO_rs source=TargetTable
 
-    'Do:ADO_rs.EOF FileNames()=ADO_rs!FileName
+  'Do:ADO_rs.EOF FileNames()=ADO_rs!FileName
 
-    'ThisFileName=dir(FolderName)  Do:ThisFileName=dir
+  'ThisFileName=dir(FolderName)  Do:ThisFileName=dir
 
-        'Chk_String.Test (Thisfilename , NameStyle)
+    'Chk_String.Test (Thisfilename , NameStyle)
 
- 		'For:If:FileNames=Thisfilename ThisFileName=""
+    'For:If:FileNames=Thisfilename ThisFileName=""
 
-      	'ADO_rs add(GroupName,FolderName,ThisFileName)
+    'ADO_rs add(GroupName,FolderName,ThisFileName)
 
-      	'FileName=ThisFileName
+    'FileName=ThisFileName
 
     
     
