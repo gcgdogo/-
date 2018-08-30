62309e6c8f138b246fb38c8fb0bbd128abb9c771
gcgdogo <gcgdogo@163.com>
2018-08-30 14:37:16 +0800

    差不多了，要开始Debug了

--- GetFileNames/GetFileNames.vb
+++ GetFileNames/GetFileNames.vb
@@ -1,31 +1,59 @@
-'识别目标 ( ID , 添加时间 , 文件组 , 文件夹 , 文件名规则 , 现存符合文件数量 , 最新发现时间 , 最新版本号 , 发现时间匹配 , 版本号匹配)
-'识别结果 ( ID , 发现时间 , 文件组 , 文件夹 , 文件名 , 符合规则 , 版本号 , 存在)
-
+'Search_Origin_Table = 识别目标 ( ID , 添加时间 , 文件组 , 文件夹 , 文件名规则 , 现存符合文件数量 , 最新发现时间 , 最新版本号 , 发现时间匹配 , 版本号匹配)
+'TargetTable = 识别结果 ( ID , 发现时间 , 文件组 , 文件夹 , 文件名 , 符合规则 , 版本号 , 存在)
 
 Function Get_File_Names(Search_Origin_Table as String , TargetTable as String ) as Boolean
     Dim Version_Changed as Boolean
-    Dim ADO_rs as ADODB.Recordset
-    Set ADO_rs = new ADODB.Recordset
-
-    'Set:ADO_rs source=Search_Origin_Table
+    Dim ADO_rs as New ADODB.Recordset
 
     Version_Changed=False
 
+    '调用 Search_File_Exist
+    Version_Changed=Version_Changed or Search_File_Exist(TargetTable)
+
+    'Set:ADO_rs source=Search_Origin_Table
+    ADO_rs.ActiveConnection=CurrentProject.Connection
+    ADO_rs.Source=Search_Origin_Table
+    ADO_rs.CursorType=adOpenStatic
+    ADO_rs.LockType=adLockReadOnly
+    ADO_rs.Open
     'Do:ADO_rs=EOF
-        call Search_Folder(ADO_rs!文件组 , ADO_rs!文件夹 , ADO_rs!文件名规则 , TargetTable)
+    Do While ADO_rs.EOF=False
+        'Private Function Search_Folder(GroupName as String , FolderName as String , Str_Expression as String , TargetTable as String ) as Boolean
+        Version_Changed=Version_Changed or Search_Folder( ADO_rs!文件组 , ADO_rs!文件夹 , ADO_rs!文件名规则 , TargetTable)
+        ADO_rs.MoveNext
+    Loop
 
-
+    Get_File_Names=Version_Changed
 End Function
 
 Private Function Search_File_Exist(TargetTable as String) as Boolean
     Dim Version_Changed as Boolean
-    Dim ADO_rs as ADODB.Recordset
-    Set ADO_rs = new ADODB.Recordset
+    Dim ADO_rs as New ADODB.Recordset
+    Dim Dir_String as String
+
+    Version_Changed=False
 
     'Set:ADO_rs source=TargetTable
+    ADO_rs.ActiveConnection=CurrentProject.Connection
+    ADO_rs.Source=TargetTable
+    ADO_rs.CursorType=adOpenDynamic
+    ADO_rs.LockType=adLockOptimistic
+    ADO_rs.Open
+    'Do:ADO_rs.EOF if ADO_rs!存在=true if Dir()="" Then ADO_rs!存在=False
+    Do While ADO_rs.EOF=False
+        if ADO_rs!存在=True Then
+            if right(ADO_rs!文件夹,1)="" then Dir_String= ADO_rs!文件夹 & ADO_rs!文件名 else Dir_String= ADO_rs!文件夹 & "\" & ADO_rs!文件名
+            if Dir(Dir_String)="" then
+                if ADO_rs!符合规则=True then Version_Changed=True
+                ADO_rs!存在=False
+                ADO_rs.Update
+            end if
+        end if
+        ADO_rs.MoveNext
+    Loop 
 
-    'Do:ADO_rs.EOF if ADO_rs!存在=true if dir()="" Then ADO_rs!存在=False
-
+    ADO_rs.Close
+    Search_File_Exist=Version_Changed
 
 End Function
 
@@ -34,14 +62,11 @@ Private Function Search_Folder(GroupName as String , FolderName as String , Str_
     Dim FileNames(500) as String , FileNamesCount as Integer , FileNamesFinger as Integer , ThisFileName as String
     Dim I as Integer
 
-    Dim ADO_rs as ADODB.Recordset
-    Dim R_Exp as RegExp , R_SubMatchs as Match.SubMatches
+    Dim ADO_rs as New ADODB.Recordset
+    Dim R_Exp as New RegExp , R_SubMatchs as Match.SubMatches
 
     Dim Str_Expression_Edited as String , VerPhaseString as String , Time_Search_Started as Variant
     Dim Version_Changed as Boolean
-
-    Set ADO_rs = new ADODB.Recordset
-    Set R_Exp as new RegExp
 
     'RE_PreEdit初始设置
     R_Exp.IgnoreCase=True
@@ -74,6 +99,7 @@ Private Function Search_Folder(GroupName as String , FolderName as String , Str_
     Do While ADO_rs.EOF=False
         FileNamesCount=FileNamesCount+1
         FileNames(FileNamesCount)=ADO_rs!文件名
+        ADO_rs.MoveNext
     Loop
 
     ADO_rs.Close
@@ -89,8 +115,8 @@ Private Function Search_Folder(GroupName as String , FolderName as String , Str_
     ADO_rs.LockType=adLockOptimistic
     ADO_rs.Open
 
-    'ThisFileName=dir(FolderName)  Do:ThisFileName=dir
-    ThisFileName=dir(FolderName)
+    'ThisFileName=Dir(FolderName)  Do:ThisFileName=Dir
+    ThisFileName=Dir(FolderName)
     Do While ThisFileName<>""
 
         'For:If:FileNames=Thisfilename ThisFileName=""
@@ -104,7 +130,7 @@ Private Function Search_Folder(GroupName as String , FolderName as String , Str_
                 Exit Function
             end if
 
-'识别结果 ( ID , 发现时间 , 文件组 , 文件夹 , 文件名 , 符合规则 , 版本号 , 存在)
+            '识别结果 ( ID , 发现时间 , 文件组 , 文件夹 , 文件名 , 符合规则 , 版本号 , 存在)
             ADO_rs.AddNew
 
             ADO_rs!发现时间=Time_Search_Started
@@ -118,12 +144,18 @@ Private Function Search_Folder(GroupName as String , FolderName as String , Str_
             if R_Exp.Test(ThisFileName)=True Then
                 ADO_rs!符合规则=True
                 Version_Changed=True
+
                 'RegExp系列的 Item 都是从零开始计数的 即范围为[ 0 ~ Count-1 ]
                 Set R_SubMatchs = R_Exp.Execute(ThisFileName).Item(0).SubMatches
+
                 For I=0 To R_SubMatchs.Count-1 
-                    '版本号小节统一成8个数字
-                    VerPhaseString=Right( "00000000" & R_SubMatchs(I) , 8 )
-                    if I=0 then ADO_rs!版本号=VerPhaseString else ADO_rs!版本号=ADO_rs!版本号 & "." & VerPhaseString
+
+                    '空值跳过,即不添加00000000
+                    if R_SubMatchs(I)<>"" then
+                        '版本号小节统一成8个数字
+                        VerPhaseString=Right( "00000000" & R_SubMatchs(I) , 8 )
+                        if I=0 then ADO_rs!版本号=VerPhaseString else ADO_rs!版本号=ADO_rs!版本号 & "." & VerPhaseString
+                    end if
                 Next
             'ADO_rs add(GroupName,FolderName,ThisFileName)
             ADO_rs.Update
@@ -131,6 +163,9 @@ Private Function Search_Folder(GroupName as String , FolderName as String , Str_
             FileNamesCount=FileNamesCount+1
             FileNames(FileNamesCount)=ThisFileName
         End If
+        ThisFileName=Dir
     Loop
+    ADO_rs.Close
+
     Search_Folder=Version_Changed
 End Function
