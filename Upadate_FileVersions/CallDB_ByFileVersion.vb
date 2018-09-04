039968495d9812e2fbf8939f4b5a95cb7938bdc2
gcgdogo <gcgdogo@163.com>
2018-09-04 15:42:21 +0800

    Diary_Function Edited

--- Upadate_FileVersions/CallDB_ByFileVersion.vb
+++ Upadate_FileVersions/CallDB_ByFileVersion.vb
@@ -6,15 +6,48 @@ Static App_HasDiary as Boolean , CallDB_HasDiary as Boolean
 Private Function Set_DBVersions_Direction() as String : Set_DBVersions_Direction = "F:\Database\数据库版本识别\DB_Versions.accdb" : End Function
 
 Function CallDB_All_in_One(GroupName as String , MacroName as String) as Variant
+	Dim Calling as Variant
+	Calling = CallDB_Connect(GroupName)
+	CallDB_Application.DoCmd.RunMacro(MacroName)
+	CallDB_Application.Quit
 End Function
 
 Function CallDB_Connect(GroupName as String) as Variant
-	
+	Dim TarDB_Direction as String , TarDB_Filename as String
+	Dim Calling as Variant
+
+	'初始值设定
+	On Error Goto CallDB_FirstSet
+		CallDB_Application.Quit
+CallDB_FirstSet:
+	On Error Goto 0
+	Set CallDB_Application = New Access.Application
+
+	'调用DBVersions,获取目标文件
+	CallDB_Application.OpenCurrentDatabase(Set_DBVersions_Direction())
+	TarDB_Direction = CallDB_Application.Run( "Get_FileVersion" , GroupName , True)
+	TarDB_Filename = CallDB_Application.Run( "Get_FileVersion" , "" , False)
+
 	'检测当前数据库是否有 Diary    On Error Goto App_Diary_Fail
-	'检测目标数据库是否有 Diary    On Error Goto CallDB_Diary_Fail
-
-	'检测 Diary 是否存在, 建立 Diary 连接
-
+	App_HasDiary = False
+	CallDB_HasDiary = False
+	On Error Goto App_Diary_Fail
+		'确保本地 DairyAdd 调用过,避免 Diary_Application 没有初始化
+		Calling = Diary_Add("Message", "[Call]>>" & GroupName & ": 获取文件版本 Get_FileVersion = " & TarDB_Filename)
+		App_HasDiary = True
+	'检测目标数据库是否有 Diary   建立 Diary 连接  On Error Goto CallDB_Diary_Fail
+		On Error Goto CallDB_Diary_Fail
+			CallDB_Application.Run("Diary_Application_Set" , Diary_Application)
+			CallDB_Application.Run("Diary_HeadString_Set" , Diary_HeadString & "[Call:" & GroupName & "] ")
+			CallDB_HasDiary = True
+CallDB_Diary_Fail:
+		On Error Goto App_Diary_Fail
+		If CallDB_HasDiary = True Then
+			Calling = Diary_Add("Message", "[Call]>>" & GroupName & ": Diary_Application_Set : Diary 更新目标设置")
+		Else
+			Calling = Diary_Add("Message", "[Call]>>" & GroupName & ": 未检测到 Diary_Application_Set")
+		End If
+	On Error Goto 0
 End Function
 
 'Example:
--- Upadate_FileVersions/Diary_Function_v2.vb
+++ Upadate_FileVersions/Diary_Function_v2.vb
@@ -1,34 +1,38 @@
 Option Compare Database
 Option Explicit
 
-Sub Diary_Application_Set(X_App as Application)
+Static Diary_Application as Application , Diary_HeadString as String
 
+Sub Diary_Application_Set(X_App as Application)
+    Set Diary_Application = X_App
 End Sub
 
-Function Diary_Application_Get() as Application
-
-End Function
-
-
 '  [Call:LangxinData][Call]
-Function Diary_HeadString() as String
-
-End Function
+Sub Diary_HeadString_Set(X_String as String)
+    Diary_HeadString = X_String
+End Sub
 
 Function Diary_Add(In_Type As String, In_Txt As String)
 
     Dim DiaryADO As ADODB.Recordset
     Set DiaryADO = New ADODB.Recordset
     Dim DiarySQL As String
+
+    '检测 Diary_Application 是否已设定 (Nothing)
+    If Diary_Application is Nothing Then Set Diary_Application = Application
+
     DiarySQL = "INSERT INTO Temp_Diary ( Type, [Time], ms, Txt ) SELECT """ & In_Type & """,now(),""" & CStr((Timer() * 1000) Mod 1000) & """ , """ & In_Txt & """;"
     DiaryADO.Source = DiarySQL
-    DiaryADO.ActiveConnection = CurrentProject.Connection
+    DiaryADO.ActiveConnection = Diary_Application.CurrentProject.Connection
     DiaryADO.Open
     
-    Application.Forms("diary").Requery
-    Application.Forms("diary").Repaint
+    Diary_Application.Forms("diary").Requery
+    Diary_Application.Forms("diary").Repaint
     
-    Do While Form_Diary.Dirty = True
+    Do While Diary_Application.Forms("diary").Dirty = True
+        DoEvents
+        DoEvents
+        DoEvents
         DoEvents
     Loop
     
