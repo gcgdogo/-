999da0298ee08d9fccf2d1a50268440034183788
gcgdogo <gcgdogo@163.com>
2018-09-05 14:14:08 +0800

    Debug

--- Upadate_FileVersions/CallDB_ByFileVersion.vb
+++ Upadate_FileVersions/CallDB_ByFileVersion.vb
@@ -42,7 +42,11 @@ CallDB_FirstSet:
 		App_HasDiary = True
 		'检测目标数据库是否有 Diary   建立 Diary 连接  On Error Goto CallDB_Diary_Fail
 		On Error Goto CallDB_Diary_Fail
-			Calling = CallDB_Application.Run("Diary_Application_Set" , Diary_Application)
+			If Diary_Application is Nothing Then
+				Calling = CallDB_Application.Run("Diary_Application_Set" , Application)
+			Else
+				Calling = CallDB_Application.Run("Diary_Application_Set" , Diary_Application)
+			End IF
 			Calling = CallDB_Application.Run("Diary_HeadString_Set" , Diary_HeadString & "[Call:" & GroupName & "] ")
 			CallDB_HasDiary = True
 CallDB_Diary_Fail:
