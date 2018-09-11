45903374f9098463459c6d666de5ba9f818affa1
gcgdogo <gcgdogo@163.com>
2018-09-11 10:17:20 +0800

    用正则表达式修正 SQL Like 匹配文本 "[" 无法正常匹配 "]" 不能转义

--- Upadate_FileVersions/Diary_Function_v2.vb
+++ Upadate_FileVersions/Diary_Function_v2.vb
@@ -45,26 +45,41 @@ Function Diary_TimeElapsed( Optional Condition_Exp as String = "" , Optional Exc
     Dim CommandTime as Variant , TimeElapsed as Double
     CommandTime = Diary_CommandTime( "*" & Condition_Exp & "*" , Exception_Exp )
     If CommandTime = "未找到对应记录" Then
-        Diary_Add("Message" , "[TimeElapsed] 未找到对应记录")
+        Diary_Add "Message" , "[TimeElapsed] 未找到对应记录"
         Exit Function
     End If
 
     TimeElapsed = ( Now() - CommandTime ) * 24 * 60 * 60
     If TimeElapsed > 7200 Then
-        Diary_Add("Message" , "[TimeElapsed] 时间过长")
+        Diary_Add "Message" , "[TimeElapsed] 时间过长"
         Exit Function
     End If
 
-    Diary_Add("Message" , "[TimeElapsed] = " & Format( TimeElapsed , "#,##0" ) & "s")
+    Diary_Add "Message" , "[TimeElapsed] = " & Format( TimeElapsed , "#,##0" ) & "s"
     
 End Function
 
+'SQL中 [ 不能正常用 like 进行匹配，需要用RegExp.Replace 进行替换,只有左中括号不能匹配，右中括号加[]会出错
 Function Diary_CommandTime(Condition_Exp as String , Exception_Exp as String) as Variant
+    Dim R_Exp as New RegExp
+    Dim Condition_Exp_Edited as String , Exception_Exp_Edited as String
+    Dim Get_CommandTime as Variant
     If Not (Diary_Application Is Nothing) Then
         Diary_CommandTime = Diary_Application.Run ("Diary_CommandTime", Diary_HeadString & Condition_Exp , Diary_HeadString & Exception_Exp)
         Exit Function
     End If
-    Diary_CommandTime = DMax("Time","Temp_Diary","Type=""Command"" and Txt Like """ & Condition_Exp & """ and Txt Not Like """ & Exception_Exp & """")
+
+    R_Exp.IgnoreCase=True
+    R_Exp.Global=True
+    R_Exp.Pattern="(\[)"
+    Condition_Exp_Edited = R_Exp.Replace(Condition_Exp,"[$1]")
+    Exception_Exp_Edited = R_Exp.Replace(Exception_Exp,"[$1]")
+
+    Get_CommandTime = DMax("Time","Temp_Diary","Type=""Command"" and Txt Like """ & Condition_Exp_Edited & """ and Txt Not Like """ & Exception_Exp_Edited & """")
+    'inputbox Prompt := "DMax" , Default := "Type=""Command"" and Txt Like """ & Condition_Exp_Edited & """ and Txt Not Like """ & Exception_Exp_Edited & """"
+    IF isnull(Get_CommandTime) then Get_CommandTime = "未找到对应记录"
+    'msgbox(Get_CommandTime)
+    Diary_CommandTime=Get_CommandTime
 End Function
 
 Sub Test()
