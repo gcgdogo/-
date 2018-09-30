0d260e2e8e0c86e368fc14e6ecf324cd6bd3f3cc
gcgdogo <gcgdogo@163.com>
2018-09-30 11:15:09 +0800

    FileName_Date 完善

--- FileName_Date/FileName_Date.vb
+++ FileName_Date/FileName_Date.vb
@@ -4,11 +4,15 @@ Option Explicit
 '附加值在另一个函数中单独计算,附加值变量在模块中定义
 '正常匹配附加在整数位,剩余字符按照1/60附加
 Dim Append_Value as Double
+Dim Append_Text as String
 
 Function FileName_Date(FileName As String) As Date
 	Dim R_Exp as new RegExp , R_SubMatchs as SubMatches
 	Dim X_Year as Integer , X_Month as Integer , X_Day as Integer
-	Dim Append_Text as String
+
+	'模块级变量重置
+	Append_Value=0
+	Append_Text=""
 
     R_Exp.IgnoreCase=True
     R_Exp.Global=False
@@ -33,22 +37,55 @@ Function FileName_Date(FileName As String) As Date
 	X_Day=CInt(R_SubMatchs(2))
 
 	Append_Text = R_SubMatchs(3)
+	Call Append_Value_Cal
 
-
-
+	FileName_Date = DateSerial(X_Year , X_Month , X_Day) + Append_Value/24
 End Function
 
+'需要识别的后缀文本列表在这里
+Private Sub Append_Value_Cal()
+	Call Append_Value_Phrase( ".Lang2XL" , 0 )
+	Call Append_Value_Phrase( "#Lang2XL" , 0 )
+	Call Append_Value_Phrase( "岗级" , 1 )
+	Call Append_Value_Phrase( "v0" , 0.5 )
+	Call Append_Value_Phrase( "v1" , 1 )
+	Call Append_Value_Phrase( "v2" , 2 )
+	Call Append_Value_Phrase( "v3" , 3 )
+	Call Append_Value_Phrase( "v4" , 4 )
+	Call Append_Value_Phrase( "v5" , 5 )
+	Call Append_Value_Phrase( "v6" , 6 )
+	Call Append_Value_Phrase( "v7" , 7 )
+	Call Append_Value_Phrase( "v8" , 8 )
+	Call Append_Value_Phrase( "v9" , 9 )
+	Call Append_Value_Phrase( "A" , 1 )
+	Call Append_Value_Phrase( "B" , 2 )
+	Call Append_Value_Phrase( "C" , 3 )
+	Call Append_Value_Phrase( "D" , 4 )
+	Call Append_Value_Phrase( "E" , 5 )
+	Call Append_Value_Phrase( "F" , 6 )
+	Call Append_Value_Phrase( "G" , 7 )
+	Call Append_Value_Phrase( "H" , 8 )
+
+	'最后将剩余字符按1/60计算
+	Append_Value = Append_Value + Len(Append_Text)/60
+End Sub
+
 '将已识别的文本剔除,处理后文本作为返回值返回
-Private Function Append_Value_Cal(Append_Text as String , Check_Text as String , Check_Value as Double) as String
+Private Sub Append_Value_Phrase(Check_Text as String , Check_Value as Double)
 	Dim R_Exp as new RegExp
     R_Exp.IgnoreCase=True
     R_Exp.Global=True
-    R_Exp.Pattern=Check_Text
-    If R_Exp.Test(Append_Text)=False Then Exit Function
 
+    '为的特殊字符增加转义字符 "([\$\(\)\[\]\{\}\.\+\?])"
+    R_Exp.Pattern="([\$\(\)\[\]\{\}\.\+\?])"
+    R_Exp.Pattern= R_Exp.Replace(Check_Text,"\$1")
 
+    If R_Exp.Test(Append_Text)=False Then Exit Sub
+
+    Append_Value = Append_Value + Check_Value
+    Append_Text = R_Exp.Replace(Append_Text , "")
     
-End Function
+End Sub
 
 '旧版算法作为备用方案
 Private Function FileName_Date_PlanB(FileName As String) As Date
